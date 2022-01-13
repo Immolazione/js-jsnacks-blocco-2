bbb9a8b075e38854998a1fe2f41c80a5aae42291
Immolazione <federico.deered@gmail.com>
2022-01-13 17:10:54 +0100

    add while on ex 5

--- js/snack5.js
+++ js/snack5.js
@@ -13,29 +13,46 @@ let array2 = [13, 53];
 console.log(array1);
 console.log(array2);
 
-// ESPLICITO ARRAY PIù LUNGO E PIù CORTO
-let longArray;
-let shortArray;
+// // ESPLICITO ARRAY PIù LUNGO E PIù CORTO
+// let longArray;
+// let shortArray;
 
-// STABILISCO QUALE ARRAY PIù CORTO E QUALE PIù LUNGO
-if (array1.length > array2.length) {
-    shortArray = array2;
-    longArray = array1;
-} else if (array1.length < array2.length){
-    shortArray = array1;
-    longArray = array2;
-}
+// // STABILISCO QUALE ARRAY PIù CORTO E QUALE PIù LUNGO
+// if (array1.length > array2.length) {
+//     shortArray = array2;
+//     longArray = array1;
+// } else if (array1.length < array2.length){
+//     shortArray = array1;
+//     longArray = array2;
+// } else {
+//     console.log('I due array sono di uguale lunghezza');
+// }
 
-// ESPLICITO DIFFERENZA
-const diff = longArray.length - shortArray.length;
-console.log(diff);
+// // ESPLICITO DIFFERENZA
+// const diff = longArray.length - shortArray.length;
+// console.log(diff);
 
-// AGGIUNGO NUMERI CASUALI FINCHè NON SONO DELLA STESSA LENGTH
-for (let i = 0; i < diff; i++) {
+// // AGGIUNGO NUMERI CASUALI FINCHè NON SONO DELLA STESSA LENGTH
+// for (let i = 0; i < diff; i++) {
+//     const rndNum = Math.floor(Math.random() * 80 + 1);
+//     shortArray.push(rndNum);
+// }
+
+// // CONSOLE DEGLI ARRAY CON LUNGHEZZA UGUALE
+// console.log(array1);
+// console.log(array2);
+
+while (array1.length !== array2.length) {
     const rndNum = Math.floor(Math.random() * 80 + 1);
-    shortArray.push(rndNum);
+
+    if (array1.length > array2.length) {
+        array2.push(rndNum);
+    } else if (array1.length < array2.length) {
+        array1.push(rndNum);
+    } else {
+        console.log('I due array sono di uguale lunghezza!');
+    }
 }
 
-// CONSOLE DEGLI ARRAY CON LUNGHEZZA UGUALE
 console.log(array1);
 console.log(array2);
