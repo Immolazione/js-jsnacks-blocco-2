b885dd52ddb95f0ae1a2eb0d1d0e62babb3f7cf8
Immolazione <federico.deered@gmail.com>
2022-01-13 16:51:33 +0100

    add ex 2

--- js/snack2.js
+++ js/snack2.js
@@ -4,3 +4,14 @@ console.log('JS OK');
 Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo
 */
+
+// INSERISCO UN NUMERO
+let num = parseInt(prompt('Inserisci un numero').trim());
+
+// SE PARI STAMPO, SE DISPARI STAMPO IL SUCCESSIVO
+
+while (num % 2 !== 0) {
+    num += 1;
+}
+
+console.log(num)
