971cbd2aa27686b1dfa71f65b3806d68ba987b9a
Immolazione <federico.deered@gmail.com>
2022-01-13 16:20:08 +0100

    add array ex 5

--- js/snack5.js
+++ js/snack5.js
@@ -5,3 +5,8 @@ Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi casuali all’array che ha meno elementi,
 fino a quando ne avrà tanti quanti l’altro.
 */
+
+//CREO DUE ARRAY
+const array1 = [12, 3, 5, 35, 23];
+const array2 = [13, 53];
+
