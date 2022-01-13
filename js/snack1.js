3331604a06c95010cf8dea77a5c2d883ec573529
Immolazione <federico.deered@gmail.com>
2022-01-13 16:36:29 +0100

    add ex 1 comments

--- js/snack1.js
+++ js/snack1.js
@@ -6,15 +6,20 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while
 */
 
+// LET SOMMA E I
 let sum = 0;
 let i = 0;
+
+// CHIEDO UN NUMERO PER 5 VOLTE
 while (i < 5) {
     const num = parseInt(prompt('Inserisci un numero').trim());
+    //VALIDAZIONE
     if (!isNaN(num)) {
         sum += num;
     }
     console.log(num);
-    
+
     i++;
 }
+// CONSOLE SOMMA
 console.log(sum);
