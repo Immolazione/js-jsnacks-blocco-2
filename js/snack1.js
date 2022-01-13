a240b19b67442433c8f9ef8e97c0c547d6734c76
Immolazione <federico.deered@gmail.com>
2022-01-13 16:34:16 +0100

    add ex 1

--- js/snack1.js
+++ js/snack1.js
@@ -5,3 +5,13 @@ Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while
 */
+
+let sum = 0;
+let i = 0;
+while (i < 5) {
+    const num = parseInt(prompt('Inserisci un numero').trim());
+    sum += num;
+    console.log(num);
+    i++;
+}
+console.log(sum);
